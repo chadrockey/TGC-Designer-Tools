2c6f6584ad0f4a74c1be04b32d167a8cdb59dc09
Chad Rockey <chadrockey@gmail.com>
2019-03-15 00:57:55 -0700

    Mark 0.2.2 Release

--- tgc_gui.py
+++ tgc_gui.py
@@ -19,7 +19,7 @@ import tgc_image_terrain
 from tgc_visualizer import drawCourseAsImage
 import OSMTGC
 
-TGC_GUI_VERSION = "0.2.1"
+TGC_GUI_VERSION = "0.2.2"
 
 image_width = 500
 image_height = 500
