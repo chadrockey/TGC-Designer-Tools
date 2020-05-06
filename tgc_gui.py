66aca01f6f3a76a3f1d76d99696d0d576119de19
Chad Rockey <chadrockey@gmail.com>
2020-05-05 21:39:29 -0700

    Tag 0.3.1 for release.

--- tgc_gui.py
+++ tgc_gui.py
@@ -20,7 +20,7 @@ import tgc_image_terrain
 from tgc_visualizer import drawCourseAsImage
 import OSMTGC
 
-TGC_GUI_VERSION = "0.3.0"
+TGC_GUI_VERSION = "0.3.1"
 
 image_width = 500
 image_height = 500
